3818e580e778efad7d1f03c17038f3980be82a35
TUAN VU <tuanvuambition@gmail.com>
2021-07-16 16:58:34 +0700

    [Bài tập] Xây dựng ứng dụng color picker

---
+++ src/app/color/color.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+import {Color} from '../color';
+
+@Component({
+  selector: 'app-color',
+  templateUrl: './color.component.html',
+  styleUrls: ['./color.component.css']
+})
+export class ColorComponent implements OnInit {
+  color: Color = {};
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  getColor(color) {
+    this.color = color;
+  }
+}
